9a37864dc8e2edce5600126f45ded50963f01651
srdjan-kolosek <srdjan@kolosek.com>
2016-08-23 00:14:12 +0200

    android app

---
+++ index.android.js
@@ -0,0 +1,6 @@
+'use strict';
+import App from './app/index';
+import React from 'react';
+import { AppRegistry } from 'react-native';
+
+AppRegistry.registerComponent('fb_gifted_chat_native', () => App);
